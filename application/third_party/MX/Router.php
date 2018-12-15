74c8c2cdbf99b2681d685aced532ae22564170ae
Ojo Tokunbo <ojo_tokunbo2002@yahoo.com>
2018-12-15 17:20:47 +0100

    setting up the db

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -235,7 +235,8 @@ class MX_Router extends CI_Router
 
 	public function set_class($class)
 	{
-		$suffix = $this->config->item('controller_suffix');
+		//$suffix = $this->config->item('controller_suffix');
+		$suffix = (string) $this->config->item('controller_suffix');
 		if (strpos($class, $suffix) === FALSE)
 		{
 			$class .= $suffix;
