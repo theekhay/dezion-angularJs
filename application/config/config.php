fc4b50c2d583a60ed6bc564700392535ff0e7aef
Ojo Tokunbo <ojo_tokunbo2002@yahoo.com>
2018-12-15 17:27:08 +0100

    configured base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/hv/'; //https://trial.mozallo.com/
+$config['base_url'] = 'https://dezion.herokuapp.com/'; //https://trial.mozallo.com/
 
 /*
 |--------------------------------------------------------------------------
